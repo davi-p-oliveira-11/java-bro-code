f94ff67383edbc9cd8862900749d32ae2ca44e9e
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-23 19:12:27 -0300

    added variables directory

--- MyFirstProgram/Main.java
+++ MyFirstProgram/Main.java
@@ -1,5 +1,19 @@
-package MyFirstProgram;
 
 public class Main {
+
+  public static void main(String[] args) {
+     
+    System.out.println("I love lasagna ");
+    System.out.println("It's really good !");
+
+    // Comment
+
+    /*
+     * Multi
+     * line
+     * comment
+     */
+
+  }
 
 }
---
+++ Variables/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+    int x = 123;
+    double y = 3.14;
+    long w = 3232313131313131313L;
+    boolean z = true;
+    char symbol = '@';
+    String name = "Bro";
+
+    System.out.println("Hello " + name);
+  }
+
+}
