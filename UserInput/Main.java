1e3a5b215f1b2ae4b3ef1508ce11405a54256266
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-23 19:53:13 -0300

    added UserInput directory

--- UserInput/Main.java
+++ UserInput/Main.java
@@ -1,5 +1,25 @@
-package UserInput;
+import java.util.Scanner;
 
 public class Main {
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("What is your name ? ");
+    String name = scanner.nextLine();
+
+    System.out.print("How old are you ? ");
+    int age = scanner.nextInt();
+    scanner.nextLine();
+
+    System.out.print("What is your favorite food ? ");
+    String food = scanner.nextLine();
+
+    System.out.println("Hallo " + name);
+    System.out.println("You are " + age + " years old");
+    System.out.println("You like " + food);
+    
+  }
 
 }
