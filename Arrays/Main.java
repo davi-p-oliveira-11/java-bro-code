2c90b8fde24afc31010a60df8f2dc285fe547800
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-23 17:28:36 -0300

    added Arrays section

---
+++ Arrays/Main.java
@@ -0,0 +1,20 @@
+
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+     // array = used to store multiple values in a single value
+
+     String[] cars = new String[3];
+
+     cars[0] = "Camaro";
+     cars[1] = "Corvette";
+     cars[2] = "Tesla";
+
+     for (int i=0; i<cars.length; i++) {
+      System.out.println(cars[i]);
+     }
+  }
+
+}
