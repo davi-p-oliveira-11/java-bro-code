10ada072d3ec2390fe7218074b0fc091c79d8b88
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-24 13:43:18 -0300

    added Wrapper folder

---
+++ WrapperClasses/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+  public static void main(String[] args) {
+    
+    // wrapper class = provides a way to use primitive data types as reference data types
+    //                 reference data types contain useful mwthods can be used with collections
+    
+    // primitive // wrapper
+    // --------- // -------
+    // boolean   Boolean
+    // char      Character
+    // int       Integer
+    // double    Double
+
+    // autoboxing = the automatic conversion that the Java compiler makes between the primitive types and their corresponding objects
+    // unboxing = the reverse of autoboxing. Automatic conversion of wrapper classes to primitive types
+
+    Boolean a = true;
+    Character b = '@';
+    Integer c = 123;
+    Double d = 3.14;
+    String e = "Bro";
+    }
+
+}
