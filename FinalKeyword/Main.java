362c8126e2b8968db7d9e45536b252ef9833d886
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-24 18:38:11 -0300

    added Objects

---
+++ FinalKeyword/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+  public static void main(String[] args) {
+     
+    // FINAL cannot be changed
+    final double PI = 3.14159;
+
+
+    System.out.println(PI);
+  }
+
+}
---
+++ Objects/Main.java
@@ -0,0 +1,24 @@
+package Objects;
+
+public class Main {
+
+  public static void main(String[] args) {
+    
+     // object = an instance of a class that may contain attriubutes and methods
+     // example: (phone, desk, computer, coffee cup)
+
+     Car myCar1 = new Car();
+     Car myCar2 = new Car();
+     
+     System.out.println(myCar1.make);
+     System.out.println(myCar1.model);
+     System.out.println();
+     System.out.println(myCar2.make);
+     System.out.println(myCar2.model);
+
+     // myCar1.drive();
+     // myCar1.brake();
+
+  }
+
+}
