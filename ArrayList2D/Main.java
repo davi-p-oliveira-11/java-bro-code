8b0ee59c2b26ea671c64c06f5bbacad8443da250
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-24 14:26:00 -0300

    added 2d arraylist

---
+++ ArrayList2D/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    // 2D Arraylist = a dynamic list of lists
+    // You can change the size of these lists during runtime
+
+    ArrayList<ArrayList<String>> groceryList = new ArrayList<>();
+    
+    ArrayList<String> bakeryList = new ArrayList<>();
+    bakeryList.add("pasta");
+    bakeryList.add("garlic bread");
+    bakeryList.add("donuts");
+
+    ArrayList<String> produceList = new ArrayList<>();
+    produceList.add("tomatoes");
+    produceList.add("zuchini");
+    produceList.add("peppers");
+
+    ArrayList<String> drinksList = new ArrayList<>();
+    drinksList.add("soda");
+    drinksList.add("coffee");
+
+    groceryList.add(bakeryList);
+    groceryList.add(produceList);
+    groceryList.add(drinksList);
+
+    System.out.println(groceryList.get(2).get(1));
+
+  }
+
+}
--- WrapperClasses/Main.java
+++ WrapperClasses/Main.java
@@ -20,6 +20,8 @@ public class Main {
     Integer c = 123;
     Double d = 3.14;
     String e = "Bro";
+
+    
     }
 
 }
