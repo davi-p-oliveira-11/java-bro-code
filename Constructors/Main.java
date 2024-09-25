b14cf4055188ce80fc180e6ec8a7aabd1fe91371
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-25 20:38:49 -0300

    added constructors

---
+++ Constructors/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  
+   public static void main(String[] args) {
+     
+    // constructor = special method that is called when a object is instantiated (created)
+
+    Human human1 = new Human("Rick", 65, 70);
+    Human human2 = new Human("Morty", 16, 50);
+
+    human2.eat();
+    human1.drink();
+
+   }
+}
