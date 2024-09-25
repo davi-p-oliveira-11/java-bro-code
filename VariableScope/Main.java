fe638425dccbeabb17fde7115af3963a65a92c2f
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-25 20:53:50 -0300

    variable scope

---
+++ VariableScope/Main.java
@@ -0,0 +1,13 @@
+package VariableScope;
+
+public class Main {
+
+  // local = declared inside a method
+  //         visible only to that method
+
+  // global = declared outside a method, but withn a class
+  //          visible to all parts of a class
+
+  DiceRoller diceRoller = new DiceRoller();
+
+}
