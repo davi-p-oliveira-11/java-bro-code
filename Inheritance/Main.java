f38c7491fb3ce242abd1f3b28b11193267ca2f63
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-09-27 16:55:23 -0300

    added Inheritance

---
+++ Inheritance/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+   public static void main(String[] args) {
+     
+    // inheritance =  the process where one class acquires
+    //                the attibute and methods of another
+
+    Car car = new Car();
+
+    car.go();
+
+    Bicycle bike = new Bicycle();
+
+    bike.stop();
+
+    System.out.println(car.doors);
+    System.out.println(bike.pedals);
+   }
+
+}
